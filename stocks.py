b794379f0be99a5e7216a869bac1d004e712f666
GermanMan <adammstrotman@gmail.com>
2021-01-05 13:06:35 -0500

    User can now input specific dates

--- stocks.py
+++ stocks.py
@@ -10,18 +10,27 @@ print('Hello! Welcome to my stock finder!')
 #While loop allows the user to search as many stocks as they wish and stop when ever they want 
 use = True
 while use == True:
+    
     #stock gets the stock and info gets what about the stock the user wants to see
     stock = str(input("What stock would you like to see: "))
     info = str(input('What information on this stock would you like to see? ie: High, Low, Open, Close: '))
-    
-    #inStart= (input(''))
-    #inEnd=
-    start = dt.datetime(2018, 1, 1)
-    end = dt.datetime(2020, 12, 31)
+
+    #lets user input specific dates
+    inYear= int(input('What year: '))
+    inMonth= int(input('What month: '))
+    inDay= int(input('What day: '))
+    endYear= int(input('What year: '))
+    endMonth= int(input('What month: '))
+    endDay= int(input('What day: '))    
+    start = dt.datetime(inYear, inMonth, inDay)
+    end = dt.datetime(endYear, endMonth, endDay)
+
     #df creates a data frame for the stock
     df = data.DataReader(stock, 'yahoo', start, end)
+
     #csv makes a new file with all the stock info so it doesn't crash  
     df.to_csv(stock+'.csv')
+
     #makes a graph for the stock
     df[[info]].plot()
     plt.show()
